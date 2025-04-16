6fa74e45f45e9643e1b7ffdb0e87d0c3dbc5782e
Franco Ezequiel Perez <francoezequielperez@MacBook-Air-de-Franco.local>
2025-04-16 17:29:53 +0200

    feat: Arguments

--- api/server.js
+++ api/server.js
@@ -1,26 +1,42 @@
-const express = require("express");
-const { ruruHTML } = require("ruru/server");
-const { graphql, buildSchema } = require("graphql");
-const { createHandler } = require("graphql-http/lib/use/express");
+const express = require('express');
+const { ruruHTML } = require('ruru/server');
+const { graphql, buildSchema } = require('graphql');
+const { createHandler } = require('graphql-http/lib/use/express');
 
 // Construct a schema, using GraphQL schema language
-const schema = buildSchema(`type Query { hello: String }`);
+const schema = buildSchema(`
+  type Query { 
+    hello(name: String!): String 
+    age: Int
+    weight: Float!
+    isOver18: Boolean
+    hobbies: [String!]!
+  }
+`);
 
 // The rootValue provides a resolver function for each API endpoint
 const rootValue = {
-  hello() {
+  hello: ({ name }) => {
     // fetch from db
     // process
     // return data
-    return "Hello world!";
+    return 'Hello ' + name;
+  },
+  age: () => {
+    return 25;
+  },
+  weight: 83,
+  isOver18: true,
+  hobbies: () => {
+    return ['Carting', 'F1', 'Simulator'];
   },
 };
 
 const app = express();
 
-app.all("/graphql", createHandler({ schema, rootValue }));
-app.get("/", (_req, res) => {
-  res.type("html"), res.end(ruruHTML({ endpoint: "/graphql" }));
+app.all('/graphql', createHandler({ schema, rootValue }));
+app.get('/', (_req, res) => {
+  res.type('html'), res.end(ruruHTML({ endpoint: '/graphql' }));
 });
 
 app.listen(4000);
