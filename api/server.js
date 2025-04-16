8d3ba30c7d8805a54ed60e2f123581ca4776f77b
Franco Ezequiel Perez <francoezequielperez@MacBook-Air-de-Franco.local>
2025-04-16 17:05:24 +0200

    feat: Add Ruru GraphiQL interface

--- api/server.js
+++ api/server.js
@@ -1,24 +1,30 @@
-const { graphql, buildSchema } = require('graphql');
- 
+const express = require("express");
+const { ruruHTML } = require("ruru/server");
+const { graphql, buildSchema } = require("graphql");
+const { createHandler } = require("graphql-http/lib/use/express");
+
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`type Query { hello: String }`);
- 
+
 // The rootValue provides a resolver function for each API endpoint
 const rootValue = {
   hello() {
     // fetch from db
     // process
     // return data
-    return 'Hello world!';
+    return "Hello world!";
   },
 };
- 
-// Run the GraphQL query '{ hello }' and print out the response
-graphql({
-  schema,
-  source: '{ hello }',
-  rootValue,
-  }).then((response) => {
-    console.log(response);
-  });
- 
+
+const app = express();
+
+app.all("/graphql", createHandler({ schema, rootValue }));
+app.get("/", (_req, res) => {
+  res.type("html"), res.end(ruruHTML({ endpoint: "/graphql" }));
+});
+
+app.listen(4000);
+console.log(`
+Api running on: http://localhost:4000
+Test: http://localhost:4000/graphql?query={hello}
+`);
